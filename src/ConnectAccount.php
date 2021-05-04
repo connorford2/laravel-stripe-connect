5f7aaae1d55f3481b5dde85f8f2c1dc5dac7aba7
Connor Ford <connor@usit.care>
2021-05-04 17:05:15 -0400

    Initial commit for Connect Account

---
+++ src/ConnectAccount.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace ConnorFord2\StripeConnect;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Contracts\Support\Jsonable;
+use Stripe\ConnectAccount as StripeConnectAccount;
+use JsonSerializable;
+
+class ConnectAccount implements Arrayable, Jsonable, JsonSerializable
+{
+    /**
+     * The Stripe model instance.
+     *
+     * @var \Illuminate\Database\Eloquent\Model
+     */
+    protected $owner;
+
+    /**
+     * The Stripe ConnectAccount instance.
+     *
+     * @var \Stripe\ConnectAccount
+     */
+    protected $connectAccount;
+
+    /**
+     * Create a new ConnectAccount instance.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $owner
+     * @param  \Stripe\ConnectAccount $connectAccount
+     * @return void
+     *
+     */
+    public function __construct($owner, StripeConnectAccount $connectAccount)
+    {
+        $this->owner = $owner;
+        $this->connectAccount = $connectAccount;
+    }
+
+    /**
+     * Delete the connet account.
+     *
+     * @return \Stripe\ConnectAccount
+     */
+    public function delete()
+    {
+        return $this->owner->removeExternalAccount($this->externalAccount);
+    }
+
+    /**
+     * Get the Stripe model instance.
+     *
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function owner()
+    {
+        return $this->owner;
+    }
+
+    /**
+     * Get the Stripe ExternalAccount instance.
+     *
+     * @return \Stripe\ConnectAccount
+     */
+    public function asStripeConnectAccount()
+    {
+        return $this->connectAccount;
+    }
+
+    /**
+     * Get the instance as an array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->asStripeConnectAccount()->toArray();
+    }
+
+    /**
+     * Convert the object to its JSON representation.
+     *
+     * @param  int  $options
+     * @return string
+     */
+    public function toJson($options = 0)
+    {
+        return json_encode($this->jsonSerialize(), $options);
+    }
+
+    /**
+     * Convert the object into something JSON serializable.
+     *
+     * @return array
+     */
+    public function jsonSerialize()
+    {
+        return $this->toArray();
+    }
+
+    /**
+     * Dynamically get values from the Stripe PaymentMethod.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    public function __get($key)
+    {
+        return $this->connectAccount->{$key};
+    }
+}
--- src/Traits/ManagesConnectAccount.php
+++ src/Traits/ManagesConnectAccount.php
@@ -2,7 +2,9 @@
 
 namespace ConnorFord2\StripeConnect\Traits;
 
+use ConnorFord2\StripeConnect\ConnectAccount;
 use ConnorFord2\StripeConnect\Exceptions\InvalidAccount;
+use ConnorFord2\StripeConnect\StripeConnect;
 
 trait ManagesConnectAccount
 {
@@ -18,7 +20,18 @@ trait ManagesConnectAccount
     }
 
     /**
-     * Determine if the customer has a Stripe account ID.
+     * Retrieve the Stripe account ID.
+     *
+     * @return string|null
+     */
+    public function setStripeConnectId($stripe_connect_id)
+    {
+        $this->stripe_connect_id = $stripe_connect_id;
+        $this->save();
+    }
+
+    /**
+     * Determine if the owner has a Stripe account ID.
      *
      * @return bool
      */
@@ -28,12 +41,64 @@ trait ManagesConnectAccount
     }
 
     /**
-     * Determine if the customer has a Stripe Connect account ID and throw an exception if not.
+     * Determine if the owner has a Stripe Connect account ID and throw an exception if not.
      *
      * @return void
      * @throws \ConnorFord2\StripeConnect\Exceptions\InvalidAccount
      */
     protected function assertAccountExists()
+    {
+        if (! $this->hasStripeConnectId()) {
+            throw InvalidAccount::notYetCreated($this);
+        }
+    }
+
+    /**
+     * Determine if the owner has a Stripe Connect account ID and then create or get the account.
+     *
+     * @return void
+     * @throws \ConnorFord2\StripeConnect\Exceptions\InvalidAccount
+     */
+    protected function createOrGetConnectAccount()
+    {
+        if ($this->hasStripeConnectId()) {
+            return $this->getConnectAccount();
+        } else {
+            return $this->createConnectAccount();
+        }
+    }
+
+    /**
+     * Determine if the owner has a Stripe Connect account ID and throw an exception if not.
+     *
+     * @return ConnectAccount
+     * @throws \ConnorFord2\StripeConnect\Exceptions\InvalidAccount
+     */
+    protected function createConnectAccount($parameters)
+    {
+        $connectAccount = StripeConnect::stripeClientInstance()->accounts->create($parameters);
+        $this->setStripeConnectId($connectAccount->id);
+        return new ConnectAccount($this, $connectAccount);
+    }
+
+    /**
+     * Retrieve the owner's Stripe Connect Account
+     *
+     * @return void
+     * @throws \ConnorFord2\StripeConnect\Exceptions\InvalidAccount
+     */
+    protected function getConnectAccount()
+    {
+        return StripeConnect::stripeClientInstance()->accounts->retrieve($this->stripeConnectId(), []);
+    }
+
+    /**
+     * Delete the owner's Stripe Connect Account
+     *
+     * @return void
+     * @throws \ConnorFord2\StripeConnect\Exceptions\InvalidAccount
+     */
+    protected function removeConnectAccount()
     {
         if (! $this->hasStripeConnectId()) {
             throw InvalidAccount::notYetCreated($this);
